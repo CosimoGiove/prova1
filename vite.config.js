660018277386bc85c21ff7aa070e0be68efba6b1
cosimogiove <cosimo18497@gmail.com>
2024-06-20 13:59:17 +0200

    feat:prova

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,28 @@ import laravel from 'laravel-vite-plugin';
 import path from 'path';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/scss/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
-    resolve: {
-        alias: {
-            '~resources': '/resources/',
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap')
-        }
+  plugins: [
+    laravel({
+      input: [
+        'resources/scss/app.scss',
+        'resources/js/app.js',
+      ],
+      refresh: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '~resources': '/resources/',
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
     },
-
+  },
+  build: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "~bootstrap/scss/bootstrap";',
+        },
+      },
+    },
+  },
 });
