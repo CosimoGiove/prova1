86691eb96f0893ddcd88729597f5dd40931bdff6
cosimogiove <cosimo18497@gmail.com>
2024-06-20 16:32:09 +0200

    bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
     <!-- Usando Vite -->
     <link rel="stylesheet" href="{{ asset('build/assets/app-hp7yFsl9.css', 'https') }}">
     <script src="{{ asset('build/assets/app-db3LA4s9js', 'https') }}"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite(['resources/js/app.js'])
 </head>
 
